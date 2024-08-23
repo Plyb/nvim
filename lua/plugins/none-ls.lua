79ca0da3f4b5a06ab2c09a3e8693e054c708110b
Zachary Wood <zwood@mbpro-307.evergreen.laika.com>
2024-08-23 16:10:33 -0700

    configure flake8 to not show virtual_text

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,6 +9,12 @@ return {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
         require("none-ls.diagnostics.flake8").with({
+          diagnostic_config = {
+            virtual_text = false,
+            signs = true,
+            update_in_insert = false,
+            underline = true,
+          },
           extra_args = {
             "--max-line-length=79",
             "ignore=...",
