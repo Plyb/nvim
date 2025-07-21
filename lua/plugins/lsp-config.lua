fce9902f15ed6f99e436fe09e05252fc1d377e27
zbroxv <zach.wood13@gmail.com>
2025-07-20 22:11:27 -0700

    remove clangd from mason-lspconfig since it initlizes the LSP twice

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,7 +11,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "clangd", "bashls" },
+        ensure_installed = { "lua_ls", "pyright", "bashls" },
       })
     end,
   },
